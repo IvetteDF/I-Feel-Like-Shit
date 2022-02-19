21e20fa07dfee2de6b693b57730530a83e4717c2
Ivette Fernandez <ivette.diana.fernandez@gmail.com>
2022-02-19 17:19:33 -0600

    Initial Commit

---
+++ I Feel Like Shit/I_Feel_Like_ShitApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_Feel_Like_ShitApp.swift
+//  I Feel Like Shit
+//
+//  Created by Ivette Fernandez on 2/19/22.
+//
+
+import SwiftUI
+
+@main
+struct I_Feel_Like_ShitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
