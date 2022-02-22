fbdc2aa542fa8228f2ac3e30256ba6f887819a91
Ivette Fernandez <ivette.diana.fernandez@gmail.com>
2022-02-21 21:51:12 -0600

    refactored to put views in their own files

---
+++ I Feel Like Shit/Views/FiguredOutView.swift
@@ -0,0 +1,21 @@
+//
+//  FiguredOutView.swift
+//  I Feel Like Shit
+//
+//  Created by Ivette Fernandez on 2/21/22.
+//
+
+import SwiftUI
+
+struct FiguredOutView: View {
+    var body: some View {
+        Text("You figured it out!")
+            .font(.title)
+    }
+}
+
+struct FiguredOutView_Previews: PreviewProvider {
+    static var previews: some View {
+        FiguredOutView()
+    }
+}
